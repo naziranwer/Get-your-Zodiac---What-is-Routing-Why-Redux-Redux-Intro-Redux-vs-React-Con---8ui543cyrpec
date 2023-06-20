3dedf77de4602eeefdafce178195b2e227746f9b
Mohd Nazir Anwer <anwernazir674@gmail.com>
2023-06-20 10:51:16 +0530

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import '../styles/App.css';
-import {useSelector,useDispatch} from "react-redux";
+import {useSelector,useDispatch,Provider} from "react-redux";
 import { onInputChange } from '../actions/index.js';
+import store from '../store';
+
 const App = () => {
 const myState = useSelector((state)=>state.tellZodiac);
 const dispatch =useDispatch();
 
   return (
+    <Provider store={store}>
     <div id="main">
       
        <div>
        <h1>Get your Zodiac </h1>
         Enter Birthday month
-        <input id='input' onChange={(e)=>dispatch(onInputChange(e.target.value))}  /><br/>
+        <input id='input1' onChange={(e)=>dispatch(onInputChange(e.target.value))}  /><br/>
         Output Zodiac
-        <input id='output' value={myState} />
+        <input id='input2' value={myState} />
        </div>
 
     </div>
+    </Provider>
   )
 }
 
